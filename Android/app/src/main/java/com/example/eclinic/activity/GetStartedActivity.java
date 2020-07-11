7ceac9207c03c03871f81cfa02e9f2a4122c6989
OmSingh <omsingh6903@gmail.com>
2020-07-11 21:06:17 +0530

    Added get started activity

--- Android/app/src/main/java/com/example/eclinic/activity/GetStartedActivity.java
+++ Android/app/src/main/java/com/example/eclinic/activity/GetStartedActivity.java
@@ -2,15 +2,39 @@ package com.example.eclinic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.eclinic.R;
+import com.example.eclinic.databinding.ActivityGetStartedBinding;
 
 public class GetStartedActivity extends AppCompatActivity {
+    ActivityGetStartedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_started);
+        binding = ActivityGetStartedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GetStartedActivity.this,DoctorLoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        binding.patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GetStartedActivity.this,PatientLoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
