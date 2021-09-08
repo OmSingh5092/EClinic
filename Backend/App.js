a77fb5d3c98a08eddf509fbeff31a42a3cb0084d
Om Singh <omsingh6903@gmail.com>
2021-09-08 16:13:39 +0530

    Changed backend DB config

--- Backend/App.js
+++ Backend/App.js
@@ -1,30 +1,33 @@
-const express=  require("express");
-const config = require('./config').app;
-const database = require('./database/database')
+const express = require("express");
+const config = require("./config").app;
+const database = require("./database/database");
 const app = express();
 
 //Applying middlewares
 app.use(express.json());
 
 //Using Passport
-require('./passport/passport');
+require("./passport/passport");
 
 //Importing routes
-const appointmentRouter = require('./routes/appointment');
-const doctorRouter = require('./routes/doctor');
-const patientRouter = require('./routes/patient');
-const googleLogin = require('./routes/googleLogin')
+const appointmentRouter = require("./routes/appointment");
+const doctorRouter = require("./routes/doctor");
+const patientRouter = require("./routes/patient");
+const googleLogin = require("./routes/googleLogin");
 
-app.use("/api/google",googleLogin);
-app.use("/api/patient",patientRouter);
-app.use("/api/appointment",appointmentRouter)
-app.use("/api/doctor",doctorRouter);
+app.use("/api/google", googleLogin);
+app.use("/api/patient", patientRouter);
+app.use("/api/appointment", appointmentRouter);
+app.use("/api/doctor", doctorRouter);
 
-//Listening 
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "APIs are working fine!",
+  });
+});
 
-database.au
-app.listen(config.local.port,()=>{
-    console.log("\n\n App listening... \n\n");
-})
-
-
+//Listening
+app.listen(config.local.port, () => {
+  console.log("\n\n App listening... \n\n");
+});
